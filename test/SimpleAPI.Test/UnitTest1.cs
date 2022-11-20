d35d7d54e2b8a6b4b98735d19d75bd76fb142258
lwang <lwang@micro-wang.com>
2022-11-20 18:30:11 +1300

    Initial Commit

---
+++ test/SimpleAPI.Test/UnitTest1.cs
@@ -0,0 +1,17 @@
+using System;
+using Xunit;
+using SimpleAPI.Controller;
+
+namespace SimpleAPI.Test
+{
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+        [Fact]
+        public void Test1()
+        {
+            
+
+        }
+    }
+}
